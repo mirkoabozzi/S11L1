55b093cb3b113354127fb24979107ef95cb3f06c
Mirko Abozzi <mirko.abozzi@gmail.com>
2024-07-08 14:27:32 +0200

    initial commit

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,14 @@
+const initalState = {
+  preferiti: [],
+};
+
+const mainReducer = (state = initalState, action) => {
+  switch (action.type) {
+    case value:
+      break;
+
+    default:
+      break;
+  }
+};
+export default mainReducer;
