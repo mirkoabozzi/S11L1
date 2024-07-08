12f210a48ecd2028ec53b80bb49b61b8b0578976
Mirko Abozzi <mirko.abozzi@gmail.com>
2024-07-08 15:26:02 +0200

    aggiunta stato da job ok

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,20 +1,34 @@
-import { Row, Col } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import { Row, Col, Button } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 
-const Job = ({ data }) => (
-  <Row
-    className="mx-0 mt-3 p-3"
-    style={{ border: '1px solid #00000033', borderRadius: 4 }}
-  >
-    <Col xs={3}>
-      <Link to={`/${data.company_name}`}>{data.company_name}</Link>
-    </Col>
-    <Col xs={9}>
-      <a href={data.url} target="_blank" rel="noreferrer">
-        {data.title}
-      </a>
-    </Col>
-  </Row>
-)
+const Job = ({ data }) => {
+  const dispatch = useDispatch();
 
-export default Job
+  return (
+    <Row className="mx-0 mt-3 p-3" style={{ border: "1px solid #00000033", borderRadius: 4 }}>
+      <Col xs={3}>
+        <Link to={`/${data.company_name}`}>{data.company_name}</Link>
+        <Button
+          variant="white"
+          className="ms-2"
+          onClick={() =>
+            dispatch({
+              type: "SELECT_COMPANY",
+              payload: data,
+            })
+          }
+        >
+          ❤️
+        </Button>
+      </Col>
+      <Col xs={9}>
+        <a href={data.url} target="_blank" rel="noreferrer">
+          {data.title}
+        </a>
+      </Col>
+    </Row>
+  );
+};
+
+export default Job;
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,22 @@
 const initalState = {
-  favourites: [],
+  favourites: {
+    content: [],
+  },
 };
 
-const mainReducer = (state = initalState, action) => {};
+const mainReducer = (state = initalState, action) => {
+  switch (action.type) {
+    case "SELECT_COMPANY":
+      return {
+        ...state,
+        favourites: {
+          ...state.favourites,
+          content: [...state.favourites.content, action.payload],
+        },
+      };
+
+    default:
+      break;
+  }
+};
 export default mainReducer;
